bfafc0e9b721ea771644929ecba0788adef70cdc
unknown <karthickr221096@gmail.com>
2022-07-16 11:34:48 +0530

    spotify api enabled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+  //using use effect hook which will run code when component load and based on a give condition
+  
+  useEffect(() =>{
+    //when anything changes in url the below code will run and call the function getTokenFromUrl which will 
+    //return hash from there we used to get the token
+    const hash = getTokenFromUrl();  
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token){
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      
+      spotify.getMe().then(user =>{
+        console.log('the user is ', user);
+      })
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+    {
+      token ? (
+        <Player />
+      ) : (
+        <Login />
+      )
+    }
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -4,6 +4,7 @@ const redirectUri = "http://localhost:3000/";
 //i got the client Id from spotify developer page
 const clientId = "16e6699504cb4bec98113ff701839c24";
 
+//defining the scope for spotify user
 const scopes = [
     "user-read-currently-playing",
     "user-read-recently-played",
@@ -11,6 +12,18 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ];
+
+//get the acces token from url 
+export const getTokenFromUrl = () => {
+    return window.location.hash
+       .substring(1)
+       .split("&")
+       .reduce((initial, item) => {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+      }, {});
+  };
 
 //the below code will create a link to hit/redirect spotify login page 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
